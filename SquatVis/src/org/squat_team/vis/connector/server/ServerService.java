0502c2fb36ed1f1a9a431d05a58838525368cb05
franksn90 <s.frankausowen@googlemail.com>
2019-04-28 09:42:39 +0200

    Fix database does not store collection references correctly

--- SquatVis/src/org/squat_team/vis/connector/server/ServerService.java
+++ SquatVis/src/org/squat_team/vis/connector/server/ServerService.java
@@ -32,10 +32,19 @@ public class ServerService {
 	private ToolConfigurationDao toolConfigurationDao;
 
 	@PostConstruct
-	public void startConnectorServer() {
-		System.out.println("Starting Connector Server");
+	public void init() {
+		startConnectorServer();;
+		setDatabaseMode();
+	}
+	
+	private void startConnectorServer() {
+		System.out.println("Starting Connector Service");
 		MainServer server = MainServer.getInstance();
 		server.start();
 		server.setRunner(this);
+	}
+	
+	private void setDatabaseMode() {
+		System.setProperty("objectdb.temp.no-detach", "true");
 	}
 }
