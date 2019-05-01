c0ccf2f04073f02178059f8dca14d23c9de951d6
franksn90 <s.frankausowen@googlemail.com>
2019-05-01 16:10:22 +0200

    Fix database configuration service not registered

--- SquatVis/src/org/squat_team/vis/data/DatabaseConfigurationService.java
+++ SquatVis/src/org/squat_team/vis/data/DatabaseConfigurationService.java
@@ -2,10 +2,12 @@ package org.squat_team.vis.data;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 /**
  * This service configures the database at the application's initialization.
  */
+@WebListener
 public class DatabaseConfigurationService implements ServletContextListener {
 
 	@Override
