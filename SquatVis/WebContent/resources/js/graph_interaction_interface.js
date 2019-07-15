9cef4f0b6a7a69e878d1688af5d987648e6ff33b
franksn90 <s.frankausowen@googlemail.com>
2019-07-15 16:07:54 +0200

    Add basic architecture view

---
+++ SquatVis/WebContent/resources/js/graph_interaction_interface.js
@@ -0,0 +1,107 @@
+function setSelectorCurrent_d3(id) {
+
+}
+
+function resetSelectorCurrent_d3(id) {
+
+}
+
+function setSelectorComparison_d3(id) {
+
+}
+
+function resetSelectorComparison_d3(id) {
+
+}
+
+function setSelectorMarked_d3(id) {
+
+}
+
+function resetSelectorMarked_d3(id) {
+
+}
+
+function setSelectorSelected_d3(id) {
+
+}
+
+function resetSelectorSelected_d3(id) {
+
+}
+
+function setAllSelectorCurrent_d3(ids) {
+
+}
+
+function resetAllSelectorCurrent_d3(ids) {
+
+}
+
+function setAllSelectorComparison_d3(ids) {
+
+}
+
+function resetAllSelectorComparison_d3(ids) {
+
+}
+
+function setAllSelectorMarked_d3(ids) {
+
+}
+
+function resetAllSelectorMarked_d3(ids) {
+
+}
+
+function setAllSelectorSelected_d3(ids) {
+
+}
+
+function resetAllSelectorSelected_d3(ids) {
+
+}
+
+function selectorLevelUp_d3(id) {
+
+}
+
+function selectorLevelUpAll_d3(ids) {
+
+}
+
+function setCandidateColor_d3(id, color) {
+
+}
+
+function selectorClearCurrent_d3() {
+
+}
+
+function selectorClearComparison_d3() {
+
+}
+
+function selectorClearMarked_d3() {
+
+}
+
+function selectorClearSelected_d3() {
+
+}
+
+function selectorMarkAllCurrent_d3() {
+
+}
+
+function selectorSelectAllComparison_d3() {
+
+}
+
+function selectorSelectAllMarked_d3() {
+
+}
+
+function selectorExportAllSelected_d3() {
+
+}
